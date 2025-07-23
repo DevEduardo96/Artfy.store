8f761de6bc4e223b9af7c742f81ac01c9ed27cbd
Seu Nome <eduardoaraujo_96@outlook.com>
2025-07-23 14:00:44 -0500

    removendo supabase config

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,5 +4,5 @@ export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
   "https://servidor-loja-digital.onrender.com";
 
-export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-export const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+//export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+//export const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
